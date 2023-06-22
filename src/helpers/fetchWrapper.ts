9e2042b348550534603c81255d249c68db07c491
Valmir Junior <jnrplay@hotmail.com>
2023-06-22 20:45:16 -0300

    feat: add fetchWrapper

---
+++ src/helpers/fetchWrapper.ts
@@ -0,0 +1,108 @@
+import { RequestInit } from "next/dist/server/web/spec-extension/request";
+import { BASE_URL } from '../constants';
+
+const controller = new AbortController();
+const { signal } = controller;
+
+function bindUrl(path: string) {
+    const url = BASE_URL
+
+    return `${url}/${path}`;
+}
+
+async function handleResponse(response: Response | void) {
+    const data = await response?.json?.();
+
+    if (!response?.ok) {
+        return Promise.reject(new Error(data?.message || "Something went wrong"));
+    }
+
+    return data;
+}
+
+async function get(
+    url: string,
+    options?: RequestInit,
+) {
+    const requestOptions = {
+        method: "GET",
+        signal,
+        ...options,
+    } as RequestInit;
+
+    if (signal.aborted) {
+        return;
+    }
+
+    const response = await fetch(
+        bindUrl(url),
+        requestOptions
+    )
+
+    return handleResponse(response);
+}
+
+async function post(
+    url: string,
+    body: unknown,
+    options?: RequestInit,
+
+) {
+    const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+        signal,
+        ...options,
+    } as RequestInit;
+
+    if (signal.aborted) {
+        return;
+    }
+
+    const response = await fetch(bindUrl(url), requestOptions);
+    return handleResponse(response);
+}
+
+async function put(url: string, body: unknown, options?: RequestInit,
+) {
+    const requestOptions = {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+        signal,
+        ...options,
+    } as RequestInit;
+
+    if (signal.aborted) {
+        return;
+    }
+
+    const response = await fetch(bindUrl(url), requestOptions);
+    return handleResponse(response);
+}
+
+async function del(url: string, options?: RequestInit,
+) {
+    const requestOptions = {
+        method: "DELETE",
+        signal,
+        ...options,
+    } as RequestInit;
+
+    if (signal.aborted) {
+        return;
+    }
+
+    const response = await fetch(bindUrl(url), requestOptions);
+
+    return handleResponse(response);
+}
+
+export const fetchWrapper = {
+    get,
+    post,
+    put,
+    del,
+    abortAll: () => controller.abort(),
+};
