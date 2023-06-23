3b04ca4def29ad3a3328d92321efd5074cc49b02
Valmir Junior <jnrplay@hotmail.com>
2023-06-23 00:00:25 -0300

    fix: rename to Condutor

--- src/pages/api/drivers.ts
+++ src/pages/api/drivers.ts
@@ -5,7 +5,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 async function drivers(req: NextApiRequest, res: NextApiResponse) {
     try {
-        const drivers: Array<Displacement> = await fetchWrapper.get("Veiculo");
+        const drivers: Array<Displacement> = await fetchWrapper.get("Condutor");
         res.send({ ok: true, drivers });
     } catch (error) {
         res.status(500).send({ ok: false, error, message: messages.drivers.error });
