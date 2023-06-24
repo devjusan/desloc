a0d52bc5eea7df7eb6b3e3de1ffc224287052730
Valmir Junior <jnrplay@hotmail.com>
2023-06-23 23:20:49 -0300

    feat: add create a new client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
-import Head from 'next/head';
-import { PageContainer } from '../css/global';
-import useFetch from '../hooks/useFetch';
-import { toastService } from '../services';
-import { CircularProgress } from '@mui/material';
-import { messages } from '../config/messages/general';
-import Item from '../components/ui/item';
-import { useRouter } from 'next/router';
-import { FC } from 'react';
-import { Client } from '../types/clients';
+import Head from "next/head";
+import { PageContainer } from "../css/global";
+import useFetch from "../hooks/useFetch";
+import { toastService } from "../services";
+import { Button, CircularProgress } from "@mui/material";
+import { messages } from "../config/messages/general";
+import Item from "../components/ui/item";
+import { useRouter } from "next/router";
+import { FC, useState } from "react";
+import { Client } from "../types/clients";
+import Input from "../components/ui/input";
+import Dialog from "../components/portals/dialog";
 
 interface Response {
   response: { clients: Client[] };
@@ -16,10 +18,12 @@ interface Response {
 }
 
 const Clients: FC = () => {
+  const [open, setOpen] = useState(false);
   const router = useRouter();
-  const { response, isLoading, error } = (useFetch(
-    'api/clients'
-  ) as unknown) as Response;
+  const { response, isLoading, error } = useFetch(
+    "api/clients"
+  ) as unknown as Response;
+  const clientsEntries = Object.entries(response?.clients[0] || ({} as Client));
 
   if (error) {
     toastService.error(messages.error.default);
@@ -27,7 +31,7 @@ const Clients: FC = () => {
 
   if (isLoading || error) {
     return (
-      <PageContainer styles={{ alignItems: 'center' }}>
+      <PageContainer styles={{ alignItems: "center" }}>
         <CircularProgress />
       </PageContainer>
     );
@@ -37,28 +41,58 @@ const Clients: FC = () => {
     <>
       <Head>
         <title>Deslocamento</title>
-        <meta name='description' content='Aplicação para deslocamento' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="Aplicação para deslocamento" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <PageContainer styles={{
-        flexDirection: 'column',
-        justifyContent: 'flex-start',
-      }}>{response?.clients.map(({ id, nome, cidade }, index) => {
-          return (
-            <Item
-              key={id}
-              cb={() => {
-                router.push(`/${id}`);
-              }}
-              description={cidade}
-              title={nome}
-              index={index}
-            />
-          );
-        })}</PageContainer>
+      <PageContainer
+        styles={{
+          flexDirection: "column",
+          justifyContent: "flex-start",
+        }}
+      >
+        <Dialog
+          title="Criar cliente"
+          description="Preencha os campos abaixo para criar um novo cliente"
+          isOpen={open}
+          setOpen={setOpen}
+          cbOnSubscribe={() => {}}
+          Content={
+            <>
+              {clientsEntries.map(([key, value]) => (
+                <Input
+                  key={key}
+                  id={key}
+                  label={key}
+                  variant="standard"
+                  value={value}
+                  onChange={() => {}}
+                />
+              ))}
+            </>
+          }
+          Trigger={() => <Button sx={{
+            my: '2rem'
+          }} variant="contained">Criar cliente</Button>}
+        />
+        <>
+          {response?.clients.map(({ id, nome, cidade }, index) => {
+            return (
+              <Item
+                key={id}
+                cb={() => {
+                  router.push(`/${id}`);
+                }}
+                description={cidade}
+                title={nome}
+                index={index}
+              />
+            );
+          })}
+        </>
+      </PageContainer>
     </>
   );
-}
+};
 
 export default Clients;
